fbcdc9fe4b42c003a9699c760fb3cc9445c1e916
Gerry Tonkin-Hill <gqt20@cam.ac.uk>
2018-10-05 11:47:54 +0100

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pairsnp",
-    version="0.0.5",
+    version="0.0.6",
     author="Gerry Tonkin-Hill",
     author_email="g.tonkinhill@gmail.com",
     description="A simple package for calculating pairwise SNP distances",
